ae1b032d9d356b5ee0323b1f7627248b7d31124a
Abel Lopes <abelflopes@gmail.com>
2024-03-19 03:00:08 +0000

    refactor: split manifest config definition & writing process

---
+++ config/manifest.ts
@@ -0,0 +1,28 @@
+import { PATH as APP_PATHS } from "../src/common/constants";
+import { packageJson } from "../config/package";
+
+/**
+ * Generates manifest json object
+ * @see https://developer.chrome.com/docs/apps/manifest
+ */
+
+export const getManifest = (icons: Record<string, string>): object => ({
+  manifest_version: 3,
+  name: packageJson.displayName,
+  version: packageJson.version,
+  description: packageJson.description,
+  icons,
+  side_panel: {
+    default_path: APP_PATHS.SIDE_PANEL,
+  },
+  content_scripts: [
+    {
+      js: [APP_PATHS.CONTENT_SCRIPT],
+      matches: ["<all_urls>"],
+    },
+  ],
+  background: {
+    service_worker: APP_PATHS.SERVICE_WORKER,
+  },
+  permissions: ["sidePanel", "contextMenus", "unlimitedStorage"],
+});
--- scripts/build-manifest.ts
+++ scripts/build-manifest.ts
@@ -1,15 +1,13 @@
 /**
  * @module
- * Generates manifest json object
- * @see https://developer.chrome.com/docs/apps/manifest
+ * Writes manifest file to dist directory
  */
 
 import { globSync } from "glob";
 import * as CONFIG_PATHS from "../config/paths";
-import { PATH as APP_PATHS } from "../src/common/constants";
 import path from "path";
 import fs from "fs";
-import { packageJson } from "../config/package";
+import { getManifest } from "../config/manifest";
 
 const distManifestPath = path.resolve(CONFIG_PATHS.distDir, "manifest.json");
 const images = globSync(path.resolve(CONFIG_PATHS.publicDir, "icon/**/*.{png,jpg,jpeg,gif,svg}"));
@@ -17,27 +15,6 @@ const images = globSync(path.resolve(CONFIG_PATHS.publicDir, "icon/**/*.{png,jpg
 const icons = Object.fromEntries(
   images.map((i) => path.relative(CONFIG_PATHS.publicDir, i)).map((i) => [path.parse(i).name, i]),
 );
-
-const getManifest = (icons: Record<string, string>): object => ({
-  manifest_version: 3,
-  name: packageJson.displayName,
-  version: packageJson.version,
-  description: packageJson.description,
-  icons,
-  side_panel: {
-    default_path: APP_PATHS.SIDE_PANEL,
-  },
-  content_scripts: [
-    {
-      js: [APP_PATHS.CONTENT_SCRIPT],
-      matches: ["<all_urls>"],
-    },
-  ],
-  background: {
-    service_worker: APP_PATHS.SERVICE_WORKER,
-  },
-  permissions: ["sidePanel", "contextMenus", "unlimitedStorage"],
-});
 
 fs.mkdirSync(path.dirname(distManifestPath), { recursive: true });
 
